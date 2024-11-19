59945ca9db701ed2c610775b424f0f6267d310ea
Emilio Soto Andrade <emiliosotoandrade256@gmail.com>
2024-11-19 15:25:09 -0300

    add: cookie con el token de acceso v2

--- auth/authRoutes.js
+++ auth/authRoutes.js
@@ -1,10 +1,10 @@
 import e from "express";
-import { authRequest, getAccessToken, handleCallback } from "./authController.js";
+import { authRequest, getUserInfo, handleCallback } from "./authController.js";
 
 const router = e.Router()
 
 router.get("/solicitar-autorizacion", authRequest)
-router.get("/token-acceso", getAccessToken)
+router.get("/token-acceso", getUserInfo)
 router.get("/inicio", handleCallback)
 
 export default router
